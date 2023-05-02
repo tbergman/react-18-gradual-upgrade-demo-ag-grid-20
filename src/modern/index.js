9a3cdfcd43f2974153907d18bf4beafb4080b1f5
tbergman <tomer.bergman@gmail.com>
2023-05-02 18:29:50 +0300

    Switch to createRoot

--- src/modern/index.js
+++ src/modern/index.js
@@ -7,16 +7,18 @@
 
 import React from 'react';
 import {StrictMode} from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
 import {Provider} from 'react-redux';
 import App from './App';
 import {store} from '../store';
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
